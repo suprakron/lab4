eb1e387185aabab1770e862835f230d808dbff7a
suprakron <suprakron15@gmail.com>
2023-01-07 11:20:20 +0700

    first commit

---
+++ js/index02.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var arena = $("#arena");
+  var playerDom = $("#player");
+  var computerDom = $("#computer");
+  var ballDom = $("#ball");
+  var userScore = $("#user-score");
+  var computerScore = $("#computer-score");
+  var gameOver = $("#gameover");
+
+  /********** โปรแกรมตามรูปที่ 2.5 ***********/
+ 
+  /********** โปรแกรมตามรูปที่ 2.6 ***********/
+ 
+  /********** โปรแกรมตามรูปที่ 2.7 ***********/
+ 
+});
